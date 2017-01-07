5e5b7132899e623355ef085b27f7422291a732dd
Surath Adhikari <surathadhikari@gmail.com>
2017-01-07 04:19:46 +0000

    middleware and json update

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(function(req,res,next){
+    if(req.headers["x-forwarded-porto"]==="http"){
+        next();
+    }else{
+        res.redirect("http://"+req.hostname+req.url);
+    }
+})
+
 app.use(express.static("public"));
 
 app.listen(process.env.PORT,process.env.IP,function(){
